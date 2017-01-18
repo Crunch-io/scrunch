4a45e7a7acfb1a4b88dfa1b7ed3e8781e3797765
Sebastian Kriems <bastoberto@gmx.de>
2017-01-18 16:00:32 +0100

    tests for _get_connection with session or env vars

--- scrunch/tests/test_utilities.py
+++ scrunch/tests/test_utilities.py
@@ -1,4 +1,16 @@
+import pytest
+import mock
+
+import scrunch
 from scrunch.variables import validate_variable_url
+# from scrunch.datasets import Dataset
+
+
+@pytest.fixture(scope='function')
+def envpatch():
+    import os
+    os.environ['CRUNCH_USERNAME'] = 'USERNAME'
+    os.environ['CRUNCH_PASSWORD'] = 'PASSWORD'
 
 
 def test_variable_url_validation():
@@ -36,3 +48,22 @@ def test_variable_url_validation():
         '/variables/b4d10b49c385aa405756fbbf572649d3/summary'
     )
     assert not validate_variable_url(var_catalog)
+
+
+class TestGetDataset(object):
+
+    @mock.patch('pycrunch.session')
+    def test_get_connection_with_session(self, session_mock):
+        assert scrunch.datasets._get_connection() == session_mock
+        # import pdb; pdb.set_trace()
+
+    @mock.patch('pycrunch.connect')
+    def test_get_connection_with_env(self, connect_mock, envpatch):
+        import os
+        user = os.environ.get('CRUNCH_USERNAME')
+        pw = os.environ.get('CRUNCH_PASSWORD')
+        assert user == 'USERNAME'
+        assert pw == 'PASSWORD'
+
+        scrunch.datasets._get_connection()
+        assert connect_mock.call_args[0] == (user, pw)
