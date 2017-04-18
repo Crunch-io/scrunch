e298b806415ad26fe77a419a231ec01224dbce29
Darwin Monroy <contact@darwinmonroy.com>
2017-04-18 11:33:07 -0600

    make the range expression for `responses_from_map` work with all supported python versions

--- scrunch/variables.py
+++ scrunch/variables.py
@@ -19,7 +19,7 @@ def validate_variable_url(url):
 
 def responses_from_map(variable, response_map, cat_names, alias, parent_alias):
     subvars = variable.resource.subvariables.by('alias')
-
+    _supported_iterable_types = (list, tuple, range)
     try:
         responses = [
             {
@@ -27,7 +27,7 @@ def responses_from_map(variable, response_map, cat_names, alias, parent_alias):
                 'alias': subvar_alias(alias, response_id),
                 'combined_ids': [
                     subvars[subvar_alias(parent_alias, sv_alias)].entity_url
-                    for sv_alias in (combined_ids if isinstance(combined_ids, (list, tuple)) else [combined_ids])
+                    for sv_alias in (combined_ids if isinstance(combined_ids, _supported_iterable_types) else [combined_ids])
                 ]
             } for response_id, combined_ids in sorted(six.iteritems(response_map))
         ]
