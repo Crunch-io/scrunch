c98bfb038d8c582e7c8bc4833c95d1a23d0d85a0
Fernando Gutierrez <fernando.gutierrez@yougov.com>
2017-12-27 16:00:27 -0500

    Reorder imports.

--- scrunch/mutable_dataset.py
+++ scrunch/mutable_dataset.py
@@ -1,12 +1,10 @@
 import json
+
 from pycrunch.shoji import wait_progress
 
 from scrunch.datasets import BaseDataset, _get_connection, _get_dataset
-from scrunch.exceptions import (
-    InvalidDatasetTypeError,
-    InvalidVariableTypeError,
-    InvalidParamError
-)
+from scrunch.exceptions import (InvalidDatasetTypeError, InvalidParamError,
+                                InvalidVariableTypeError)
 from scrunch.expressions import parse_expr, process_expr
 from scrunch.helpers import shoji_entity_wrapper
 
