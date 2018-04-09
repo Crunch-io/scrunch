d9f2559d0c07575c6f320acb7b26cc25c81abdaf
Jj <jjdelc@gmail.com>
2018-04-09 16:07:25 -0500

    Add folders integration tests, no code yet

---
+++ integration/test_folders.py
@@ -0,0 +1,92 @@
+# coding: utf-8
+
+"""
+Tests that the variable Folders API is properly suported
+"""
+
+
+
+import os
+from unittest import TestCase
+
+from scrunch import connect
+from scrunch import get_dataset
+from fixtures import NEWS_DATASET
+
+HOST = os.environ['SCRUNCH_HOST']
+username = os.environ['SCRUNCH_USER']
+password = os.environ['SCRUNCH_PASS']
+
+site = connect(username, password, HOST)
+
+
+def setup_folders(ds):
+    sf1 = ds.folders.post({
+        'body': {
+            'name': 'Subfolder 1'
+        }
+    })
+    sfa = sf1.post({
+        'body': {
+            'name': 'Subfolder A'
+        }
+    })
+    sf2 = ds.folders.post({
+        'body': {
+            'name': 'Subfolder 2'
+        }
+    })
+    variables = ds.variables.by('alias')
+    sf1.patch({'index': {
+        variables['age'].self: {}
+    }})
+    sfa.patch({'index': {
+        variables['gender']: {}
+    }})
+    sf2.patch({'index': {
+        variables['socialmedia']: {}
+    }})
+
+
+class TestFolders(TestCase):
+    def setUp(self):
+        self._ds = site.datasets.create({
+            'element': 'shoji:entity',
+            'body': {
+                'name': 'test_folders',
+                'table': {
+                    'element': 'crunch:table',
+                    'metadata': NEWS_DATASET
+                },
+            }
+        }).refresh()
+        ds = self._ds
+        ds.settings.edit(variable_folders=True)
+        ds.refresh()
+        setup_folders(ds)
+        self.ds = get_dataset(ds.body.id)
+
+    def test_get_folders(self):
+        ds = self.ds
+        root = ds.folders.get('|')
+        sf1 = ds.folders.get('| Subfolder 1')
+        sfa = ds.folders.get('| Subfolder 1 | Subfolder A')
+        self.assertEqual(sf1.name, 'Subfolder 1')
+        self.assertEqual(sfa.name, 'Subfolder A')
+        self.assertEqual(sf1.parent, root)
+        self.assertEqual(sfa.parent, sf1)
+
+    def test_hidden_folder(self):
+        assert False
+
+    def test_hide_variable(self):
+        assert False
+
+    def test_make_subfolder(self):
+        assert False
+
+    def test_reorder_folder(self):
+        assert False
+
+    def test_move_between_folders(self):
+        assert False
