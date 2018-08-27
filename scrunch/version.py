13da4239b4adb6bf2cf2150fcc4bd42023b0248d
Neal Richardson <neal@crunch.io>
2018-08-27 10:01:12 -0700

    Report a useful default version

--- scrunch/version.py
+++ scrunch/version.py
@@ -4,5 +4,5 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-    # package is not installed
-    __version__ = 'source'
+    # Package is installed from source. It's at least this version
+    __version__ = '0.6.3-uninstalled'
