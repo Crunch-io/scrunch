c3d1cabb89221b60113edf157492d3bfe226c958
Jj <jjdelc@gmail.com>
2018-10-10 18:28:32 -0500

    Update test to new projects .refresh()es

--- scrunch/tests/mock_session.py
+++ scrunch/tests/mock_session.py
@@ -16,19 +16,27 @@ class FixtureAdapter(BaseAdapter):
     def __init__(self):
         self.fixtures = {}
         self.requests = []
+        self.post_responses = []
 
     def add_fixture(self, url, fixture):
         self.fixtures[url] = fixture
 
+    def add_post_response(self, response):
+        self.post_responses.append(response)
+
     def send(self, request, **kwargs):
         self.requests.append(request)
-        url = request.url
-        if url not in self.fixtures:
-            raise NotImplementedError("URL: %s does not have a fixture" % url)
-        response = Response()
-        response.status_code = 200
-        response.headers['Content-Type'] = 'application/json'
-        response.raw = BytesIO(json.dumps(self.fixtures[url]).encode('utf-8'))
+        if request.method == 'POST':
+            response = self.post_responses.pop(0)
+        else:
+            url = request.url
+            if url not in self.fixtures:
+                raise NotImplementedError(
+                    "URL: %s does not have a fixture" % url)
+            response = Response()
+            response.status_code = 200
+            response.headers['Content-Type'] = 'application/json'
+            response.raw = BytesIO(json.dumps(self.fixtures[url]).encode('utf-8'))
         return response
 
 
@@ -42,6 +50,9 @@ class MockSession(ScrunchSession):
 
     def add_fixture(self, url, fixture):
         self.adapter.add_fixture(url, fixture)
+
+    def add_post_response(self, response):
+        self.adapter.add_post_response(response)
 
     def get_fixture(self, url):
         return self.adapter.fixtures[url]
