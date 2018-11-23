d3e690c9bd727cffd8da06611286673d475fa81d
Mathias Bustamante <mathiasbc@gmail.com>
2018-11-23 09:55:52 -0500

    parsing filter for crtabs and allow weight to be passed a Variable url, instance or alias

--- scrunch/cubes.py
+++ scrunch/cubes.py
@@ -1,5 +1,6 @@
 from pycrunch.cubes import fetch_cube, count
 from scrunch.datasets import Variable
+from scrunch.expressions import parse_expr, process_expr
 from cr.cube.crunch_cube import CrunchCube
 
 
@@ -8,10 +9,21 @@ def crtabs(dataset, variables, weight=None, filter=None, **measures):
 
     :param dataset: Dataset shoji object
     :param variables: List of variable urls, names or aliases
+    :param weight: Scrunch variable instance, alias or url
+    :param filter: Scrunch expression filter
     """
-    dataset = dataset.resource
     variables = prepare_variables(variables)
-    return CrunchCube(fetch_cube(dataset, variables, count=count(), weight=weight, filter=filter, **measures))
+    if weight:
+        if isinstance(weight, Variable):
+            weight = weight.url
+        elif isinstance(weight, six.string_types):
+            if weight in dataset.keys():
+                weight = dataset[weight].url
+    dataset = dataset.resource
+    if filter:
+        filter = process_expr(parse_expr(filter), dataset)
+    return CrunchCube(fetch_cube(
+        dataset, variables, count=count(), weight=weight, filter=filter, **measures))
 
 
 def prepare_variables(variables):
