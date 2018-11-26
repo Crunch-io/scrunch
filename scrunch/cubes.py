74d492fc7ae94d52796e05ad4def11837d8934e9
Mathias Bustamante <mathiasbc@gmail.com>
2018-11-26 09:24:40 -0500

    standarizing variable conversion to url and cleaning code a little bit

--- scrunch/cubes.py
+++ scrunch/cubes.py
@@ -6,32 +6,31 @@ from scrunch.expressions import parse_expr, process_expr
 from cr.cube.crunch_cube import CrunchCube
 
 
+def variable_to_url(variable, ds):
+    """
+    Get a variable alias, instance or url and return
+    the url
+    """
+    if isinstance(variable, Variable):
+        return variable.url
+    elif isinstance(variable, six.string_types):
+        if variable in ds.keys():
+            return ds[variable].url
+    return variable
+
+
 def crtabs(dataset, variables, weight=None, filter=None, **measures):
     """Return CrunchCube representation of crosstab.
 
-    :param dataset: Dataset shoji object
-    :param variables: List of variable urls, names or aliases
+    :param dataset: Dataset instance
+    :param variables: List of variable urls, aliases or Variable instances
     :param weight: Scrunch variable instance, alias or url
-    :param filter: Scrunch expression filter
+    :param filter: Scrunch filter expression
     """
-    variables = prepare_variables(variables)
+    variables = [variable_to_url(var, dataset) for var in variables]
     if weight:
-        if isinstance(weight, Variable):
-            weight = weight.url
-        elif isinstance(weight, six.string_types):
-            if weight in dataset.keys():
-                weight = dataset[weight].url
-        else:
-            raise KeyError('{} not found in dataset'.format(weight))
-    dataset = dataset.resource
+        weight = variable_to_url(weight, dataset)
     if filter:
-        filter = process_expr(parse_expr(filter), dataset)
+        filter = process_expr(parse_expr(filter), dataset.resource)
     return CrunchCube(fetch_cube(
-        dataset, variables, count=count(), weight=weight, filter=filter, **measures))
-
-
-def prepare_variables(variables):
-    return [
-        v.url if type(v) is Variable else v
-        for v in variables
-    ]
+        dataset.resource, variables, count=count(), weight=weight, filter=filter, **measures))
