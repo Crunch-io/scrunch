46f56ee655731166a17cdf3047b2372806bc9eec
Jj <jjdelc@gmail.com>
2019-04-16 17:25:24 -0500

    Add a way to access the secure folder in Scrunch

--- scrunch/tests/test_folders.py
+++ scrunch/tests/test_folders.py
@@ -1,7 +1,10 @@
 from mock import MagicMock
 from scrunch.folders import Folder
+from scrunch.mutable_dataset import MutableDataset
+from pycrunch.shoji import Entity, Catalog
 
 from .test_datasets import AttributeDict
+from .mock_session import MockSession
 
 
 def _getitem(var):
@@ -22,3 +25,130 @@ def test_move_here_simple():
     folder = Folder(entity_mock, root_mock, MagicMock())
     folder.move_here(['foo', 'bar'])
     entity_mock.patch.assert_called()
+
+
+def test_unique_folders():
+    session = MockSession()
+    dataset_url = 'http://host/api/datasets/abc/'
+    folders_url = 'http://host/api/datasets/abc/folders/'
+    hidden_url = 'http://host/api/datasets/abc/folders/hidden/'
+    secure_url = 'http://host/api/datasets/abc/folders/secure/'
+    trash_url = 'http://host/api/datasets/abc/folders/trash/'
+    dataset_resource = Entity(session, **{
+        "element": "shoji:entity",
+        "self": dataset_url,
+        "body": {
+            "name": "test_dataset_project"
+        },
+        "catalogs": {
+            "folders": folders_url,
+        }
+    })
+    dataset_resource.variables = MagicMock()
+    dataset_resource.settings = MagicMock()
+    folders_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": folders_url,
+        "index": {},
+        "body": {
+            "name": "Root"
+        },
+        "catalogs": {
+            "hidden": hidden_url,
+            "secure": secure_url,
+            "trash": trash_url,
+        }
+    })
+    hidden_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": hidden_url,
+        "index": {},
+        "body": {
+            "name": "Hidden"
+        },
+    })
+    secure_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": secure_url,
+        "index": {},
+        "body": {
+            "name": "Secure"
+        },
+    })
+    trash_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": trash_url,
+        "index": {},
+        "body": {
+            "name": "Trash"
+        },
+    })
+    session.add_fixture(folders_url, folders_resource)
+    session.add_fixture(hidden_url, hidden_resource)
+    session.add_fixture(secure_url, secure_resource)
+    session.add_fixture(trash_url, trash_resource)
+    dataset = MutableDataset(dataset_resource)
+
+    assert dataset.folders.root.name == "Root"
+    assert dataset.folders.hidden.name == "Hidden"
+    assert dataset.folders.secure.name == "Secure"
+    assert dataset.folders.trash.name == "Trash"
+
+
+def test_unique_folders_no_secure():
+    session = MockSession()
+    dataset_url = 'http://host/api/datasets/abc/'
+    folders_url = 'http://host/api/datasets/abc/folders/'
+    hidden_url = 'http://host/api/datasets/abc/folders/hidden/'
+    trash_url = 'http://host/api/datasets/abc/folders/trash/'
+    dataset_resource = Entity(session, **{
+        "element": "shoji:entity",
+        "self": dataset_url,
+        "body": {
+            "name": "test_dataset_project"
+        },
+        "catalogs": {
+            "folders": folders_url,
+        }
+    })
+    dataset_resource.variables = MagicMock()
+    dataset_resource.settings = MagicMock()
+    folders_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": folders_url,
+        "index": {},
+        "body": {
+            "name": "Root"
+        },
+        "catalogs": {
+            "hidden": hidden_url,
+            # Viewer users don't have the secure folder available
+            # "secure": secure_url,
+            "trash": trash_url,
+        }
+    })
+    hidden_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": hidden_url,
+        "index": {},
+        "body": {
+            "name": "Hidden"
+        },
+    })
+    trash_resource = Catalog(session, **{
+        "element": "shoji:catalog",
+        "self": trash_url,
+        "index": {},
+        "body": {
+            "name": "Trash"
+        },
+    })
+    session.add_fixture(folders_url, folders_resource)
+    session.add_fixture(hidden_url, hidden_resource)
+    session.add_fixture(trash_url, trash_resource)
+    dataset = MutableDataset(dataset_resource)
+
+    assert dataset.folders.root.name == "Root"
+    assert dataset.folders.hidden.name == "Hidden"
+    assert dataset.folders.trash.name == "Trash"
+    assert not hasattr(dataset.folders, "secure")
