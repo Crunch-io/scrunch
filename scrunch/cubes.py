5513980a7a3e50d8a87ff19368092b2d8dccfaae
Slobodan Ilic <slobodan@crunch.io>
2020-02-24 17:14:54 +0100

    rfctr: run black and rfctr `filter` to `filter_`

--- scrunch/cubes.py
+++ scrunch/cubes.py
@@ -19,19 +19,20 @@ def variable_to_url(variable, dataset):
     return variable
 
 
-def crtabs(dataset, variables, weight=None, filter=None, transforms=None, **measures):
+def crtabs(dataset, variables, weight=None, filter_=None, transforms=None, **measures):
     """Return Cube representation of crosstab.
 
     :param dataset: Dataset instance
     :param variables: List of variable urls, aliases or Variable instances
     :param weight: Scrunch variable instance, alias or url
-    :param filter: Scrunch filter expression
+    :param filter_: Scrunch filter expression
+    :param transforms: cr.cube transforms dictionary
     """
     variables = [variable_to_url(var, dataset) for var in variables]
     if weight is not None:
         weight = variable_to_url(weight, dataset)
-    if filter is not None:
-        filter = process_expr(parse_expr(filter), dataset.resource)
+    if filter_ is not None:
+        filter_ = process_expr(parse_expr(filter_), dataset.resource)
 
     return Cube(
         fetch_cube(
@@ -39,7 +40,7 @@ def crtabs(dataset, variables, weight=None, filter=None, transforms=None, **meas
             variables,
             count=count(),
             weight=weight,
-            filter=filter,
+            filter=filter_,
             **measures
         ),
         transforms=transforms,
