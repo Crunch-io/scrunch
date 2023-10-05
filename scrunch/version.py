9d3069bfdf248a21656694ff436963a22f39072a
Jj <jjdelc@gmail.com>
2023-10-05 15:39:36 +0100

    Bump version

--- scrunch/version.py
+++ scrunch/version.py
@@ -5,4 +5,4 @@ try:
     __version__ = version('scrunch')
 except Exception:
     # Package is installed from source. It's at least this version
-    __version__ = '0.15.3-uninstalled'
+    __version__ = '0.16.0-uninstalled'
