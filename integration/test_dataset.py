89cafcc7f44ee9bbfc49bcb92bbac4495b693d06
Alessio Izzo <alessio.izzo@crunch.io>
2024-06-18 17:34:33 +0200

    fixing tests on append_dataset

--- integration/test_dataset.py
+++ integration/test_dataset.py
@@ -310,20 +310,10 @@ class TestDatasetMethods(TestCase):
         # affected by the exclusion filter, the result should be only one record.
         filter_value = "2024-03-15T00:00:00.393"
         try:
-            resp = scrunch_dataset.append_dataset(
+            scrunch_dataset.append_dataset(
                 scrunch_dataset_to_append,
                 filter="my_datetime_var > '{}'".format(filter_value)
             )
-            assert resp['body']['filter'] == {
-                'args': [
-                    {
-                        'variable': datetime_append_var['self']
-                    }, {
-                        'value': filter_value
-                    }
-                ],
-                'function': '>'
-            }
             data = ds.follow("table", "limit=20")['data']
             datetime_values = data[datetime_var.body.id]
             # We should have 5 (original dataset) + 1 (filtered in append_dataset)
@@ -341,52 +331,7 @@ class TestDatasetMethods(TestCase):
             ds.delete()
             ds_to_append.delete()
 
-    def test_append_dataset_with_filter_numeric(self):
-        ds = site.datasets.create(as_entity({"name": "test_scrunch_append_dataset"})).refresh()
-        numeric_var = ds.variables.create(
-            as_entity(
-                {
-                    "name": "my_var",
-                    "alias": "my_var",
-                    "type": "numeric",
-                    "values": [1, 2, 3, None, 5],
-                }
-            )
-        ).refresh()
-
-        ds_to_append = site.datasets.create(as_entity({"name": "test_scrunch_dataset_to_append"})).refresh()
-        ds_to_append.variables.create(
-            as_entity(
-                {
-                    "name": "my_var",
-                    "alias": "my_var",
-                    "type": "numeric",
-                    "values": [11, 21, 31, 41, 51],
-                }
-            )
-        ).refresh()
-
-        scrunch_dataset = get_mutable_dataset(ds.body.id, site)
-        scrunch_dataset_to_append = get_mutable_dataset(ds_to_append.body.id, site)
-        # This is intended to leave only one records.
-        filter_value = 50
-        try:
-            scrunch_dataset.append_dataset(
-                scrunch_dataset_to_append,
-                filter="my_var > {}".format(filter_value)
-            )
-
-            data = ds.follow("table", "limit=20")['data']
-            numeric_values = data[numeric_var.body.id]
-            # We should have 5 (original dataset) + 2 (filtered in append_dataset)
-            assert len(numeric_values) == 6
-            assert numeric_values == [1, 2, 3, {'?': -1}, 5, 51]
-        finally:
-            # cleanup
-            ds.delete()
-            ds_to_append.delete()
-
-    def test_append_dataset_with_variables_list(self):
+    def test_append_dataset_with_variables_list_and_exclusion(self):
         ds = site.datasets.create(as_entity({"name": "test_scrunch_append_dataset_with_variable_exclusion"})).refresh()
         ds.variables.create(
             as_entity(
@@ -475,6 +420,105 @@ class TestDatasetMethods(TestCase):
                 "2024-01-03T20:00:52.333",
                 "2024-02-03T20:00:52.234",
                 "2024-03-03T20:00:52.456",
+                "2024-04-03T20:00:52.999",
+            ]
+        finally:
+            # cleanup
+            ds.delete()
+            ds_to_append.delete()
+
+    def test_append_dataset_with_variables_list_filters_and_exclusion(self):
+        ds = site.datasets.create(as_entity({
+            "name": "test_scrunch_append_dataset_with_variable_filters_exclusion"
+        })).refresh()
+        ds.variables.create(
+            as_entity(
+                {
+                    "name": "my_var",
+                    "alias": "my_var",
+                    "type": "numeric",
+                    "values": [1, 2, 3, None, 5],
+                }
+            )
+        ).refresh()
+        datetime_var = ds.variables.create(
+            as_entity(
+                {
+                    "name": "my_datetime_var",
+                    "alias": "my_datetime_var",
+                    "type": "datetime",
+                    "resolution": "ms",
+                    "values": [
+                        "2023-01-03T20:00:52.333",
+                        "2023-02-03T20:00:52.234",
+                        "2023-03-03T20:00:52.456",
+                        "2023-04-03T20:00:52.999",
+                        "2023-06-03T20:00:52.123",
+                    ],
+                }
+            )
+        ).refresh()
+        ds_to_append = site.datasets.create(
+            as_entity(
+                {
+                    "name": "test_scrunch_dataset_with_exclusion_to_append"
+                }
+            )
+        ).refresh()
+        ds_to_append.variables.create(
+            as_entity(
+                {
+                    "name": "my_var",
+                    "alias": "my_var",
+                    "type": "numeric",
+                    "values": [11, 21, 31, 41, 51],
+                }
+            )
+        ).refresh()
+        ds_to_append.variables.create(
+            as_entity(
+                {
+                    "name": "my_datetime_var",
+                    "alias": "my_datetime_var",
+                    "type": "datetime",
+                    "resolution": "ms",
+                    "values": [
+                        "2024-01-03T20:00:52.333",
+                        "2024-02-03T20:00:52.234",
+                        "2024-03-03T20:00:52.456",
+                        "2024-04-03T20:00:52.999",
+                        "2024-06-03T20:00:52.123",
+                    ],
+                }
+            )
+        ).refresh()
+
+        scrunch_dataset = get_mutable_dataset(ds.body.id, site)
+        scrunch_dataset_to_append = get_mutable_dataset(ds_to_append.body.id, site)
+
+        # The exclusion is applied to the dataset to append and here
+        # the latest/oldest value is affected
+        exclusion = "my_datetime_var >= '2024-06-01T00:00:52.123'"
+        scrunch_dataset_to_append.exclude(exclusion)
+        # This is intended to leave only two records.Since the one of them is also
+        # affected by the exclusion filter, the result should be only one record.
+        filter_value = "2024-03-15T00:00:00.393"
+        try:
+            scrunch_dataset.append_dataset(
+                scrunch_dataset_to_append,
+                variables=["my_var", "my_datetime_var"],
+                filter="my_datetime_var > '{}'".format(filter_value)
+            )
+            data = ds.follow("table", "limit=20")['data']
+            datetime_values = data[datetime_var.body.id]
+            # We should have 5 (original dataset) + 1 (filtered by exclusion and filter in append_dataset)
+            assert len(datetime_values) == 6
+            assert datetime_values == [
+                "2023-01-03T20:00:52.333",
+                "2023-02-03T20:00:52.234",
+                "2023-03-03T20:00:52.456",
+                "2023-04-03T20:00:52.999",
+                "2023-06-03T20:00:52.123",
                 "2024-04-03T20:00:52.999",
             ]
         finally:
