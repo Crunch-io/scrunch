03534e1ca77eaf0ed46e3348b87071ea713fb1ff
Alessio Izzo <alessio.izzo@crunch.io>
2024-06-21 14:58:15 +0200

    refactor tests

--- integration/test_expressions.py
+++ integration/test_expressions.py
@@ -66,7 +66,7 @@ class TestExpressions(TestCase):
         scrunch_dataset = get_mutable_dataset(ds.body.id, self.site)
         return ds, scrunch_dataset
 
-    def test_mr_any_add_filter(self):
+    def test_multiple_response_any_add_filter(self):
         ds_rows = [
             ["case_id", "age", "news_source_1", "news_source_2", "news_source_3"],
             [1, 25, 1, 2, 1],
@@ -81,40 +81,33 @@ class TestExpressions(TestCase):
         finally:
             # cleanup
             ds.delete()
-
-    def test_mr_any_on_value_(self):
-        ds_rows = [
-            ["case_id", "age", "news_source_1", "news_source_2", "news_source_3"],
-            [1, 25, 1, 2, 1],
-            [2, 41, 1, 2, 2],
-            [3, 33, 1, 1, 1]
+    
+    def test_categorical_any_add_filter(self):
+        ds = self.site.datasets.create(as_entity({"name": "test_any_categorical_add_filter"})).refresh()
+        categories = [
+            {"id": 1, "name": "One", "missing": False, "numeric_value": None},
+            {"id": 2, "name": "Two", "missing": False, "numeric_value": None},
+            {"id": 3, "name": "Three", "missing": False, "numeric_value": None},
+            {"id": -1, "name": "No Data", "missing": True, "numeric_value": None}
         ]
-        ds_to_append_rows = [
-            ["case_id", "age", "news_source_1", "news_source_2", "news_source_3"],
-            [4, 10, 2, 1, 2],
-            [5, 11, 2, 1, 1],
-            [6, 12, 1, 2, 2]
-        ]
-        ds, scrunch_dataset = self._create_mr_dataset('test_mr_any', ds_rows)
-        ds_to_append, scrunch_dataset_to_append = self._create_mr_dataset('test_mr_any_to_append', ds_to_append_rows)
-        # This filter should get only the rows that have the news_source variable with the value 1
-        _filter = "news_source.any([1])"
+        ds.variables.create(as_entity({
+            "name": "my_cat",
+            "alias": "my_cat",
+            "type": "categorical",
+            "categories": categories,
+            "values": [
+                1, 2, 3, -1, -1, -1, 1, 2, 1
+            ]
+        }))
+        scrunch_dataset = get_mutable_dataset(ds.body.id, self.site)
+        _filter = "my_cat.any(1)"
         try:
-            scrunch_dataset.append_dataset(scrunch_dataset_to_append, filter=_filter)
-            ds_variables = ds.variables.by("alias")
-            age_variable_id = ds_variables["age"].id
-            case_id_variable_id = ds_variables["case_id"].id
-            news_source_variable_id = ds_variables["news_source"].id
-            data = ds.follow("table", "limit=20")['data']
-            assert data[case_id_variable_id] == [1.0, 2.0, 3.0, 6.0]
-            assert data[age_variable_id] == [25.0, 41.0, 33.0, 12.0]
-            assert data[news_source_variable_id] == [[1, 2, 1], [1, 2, 2], [1, 1, 1], [1, 2, 2]]
+            resp = scrunch_dataset.add_filter(name='filter_1', expr=_filter)
         finally:
             # cleanup
             ds.delete()
-            ds_to_append.delete()
 
-    def test_mr_any_on_subvar(self):
+    def test_any_filter_multiple_response(self):
         ds_rows = [
             ["case_id", "age", "news_source_1", "news_source_2", "news_source_3"],
             [1, 25, 1, 2, 1],
@@ -134,7 +127,7 @@ class TestExpressions(TestCase):
         # at the same time for both news_source_1 and news_source_2
         _filter = "news_source.any([news_source_1, news_source_2])"
         try:
-            resp = scrunch_dataset.append_dataset(scrunch_dataset_to_append, filter=_filter)
+            scrunch_dataset.append_dataset(scrunch_dataset_to_append, filter=_filter)
             ds_variables = ds.variables.by("alias")
             age_variable_id = ds_variables["age"].id
             case_id_variable_id = ds_variables["case_id"].id
@@ -148,7 +141,7 @@ class TestExpressions(TestCase):
             ds.delete()
             ds_to_append.delete()
 
-    def test_categorical_any_on_subvar(self):
+    def test_any_filter_categorical(self):
         ds = self.site.datasets.create(as_entity({"name": "test_any_category"})).refresh()
         ds_to_append = self.site.datasets.create(as_entity({"name": "test_any_category_to_append"})).refresh()
         categories = [
