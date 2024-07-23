fe224168a7dbc031829d6bbac83cea7c8d02b656
Alessio Izzo <alessio.izzo@crunch.io>
2024-07-23 11:24:08 +0200

    version bump 0.18.4 (#461)

--- scrunch/version.py
+++ scrunch/version.py
@@ -10,4 +10,4 @@ try:
     __version__ = version('scrunch')
 except Exception:
     # Package is installed from source. It's at least this version
-    __version__ = '0.18.3-uninstalled'
+    __version__ = '0.18.4-uninstalled'
