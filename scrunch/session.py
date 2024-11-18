d7040e5027d97d64f0177df9ad3dccca737bae8f
Angelo Romano <angelo@crunch.io>
2024-11-18 12:30:08 +0100

    [#188587672]: Adjustments and enhancements

--- scrunch/session.py
+++ scrunch/session.py
@@ -1,8 +1,17 @@
-from pycrunch.version import __version__ as pycrunch_version
+import os
+
 from pycrunch import connect as _connect
 from pycrunch.elements import ElementSession
+from pycrunch.version import __version__ as pycrunch_version
 
 from .version import __version__
+
+SSL_UNSAFE = os.environ.get("SSL_UNSAFE", "").strip().lower() in (
+    "1",
+    "true",
+    "yes",
+    "y",
+)
 
 
 class ScrunchSession(ElementSession):
@@ -16,6 +25,7 @@ class ScrunchSSLUnsafeSession(ScrunchSession):
     A subclass of `ScrunchSession` that skips SSL certificate validation
     when trying to connect to the API server. Useful for local testing.
     """
+
     def __init__(self, *args, **kwargs):
         super(ScrunchSSLUnsafeSession, self).__init__(*args, **kwargs)
         self.verify = False
@@ -26,15 +36,17 @@ FLAGS_TO_CHECK = {"old_projects_order", "clients_strict_subvariable_syntax"}
 
 def set_feature_flags(site):
     feature_flags = {
-        flag_name: site.follow('feature_flag',
-                               'feature_name=%s' % flag_name).value['active']
+        flag_name: site.follow("feature_flag", "feature_name=%s" % flag_name).value[
+            "active"
+        ]
         for flag_name in FLAGS_TO_CHECK
     }
-    setattr(site.session, 'feature_flags', feature_flags)
+    setattr(site.session, "feature_flags", feature_flags)
     return site
 
 
 def connect(*args, **kwargs):
-    _site = _connect(session_class=ScrunchSession, *args, **kwargs)
+    session_class = ScrunchSSLUnsafeSession if SSL_UNSAFE else ScrunchSession
+    _site = _connect(session_class=session_class, *args, **kwargs)
     _site = set_feature_flags(_site)
     return _site
