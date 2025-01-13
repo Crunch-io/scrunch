be060c08657f12b914073d2777420d2f870a0bad
Raul Chacon <raul.chacon@crunch.io>
2025-01-13 14:00:49 -0800

    Update version.py

--- scrunch/version.py
+++ scrunch/version.py
@@ -10,4 +10,4 @@ try:
     __version__ = version('scrunch')
 except Exception:
     # Package is installed from source. It's at least this version
-    __version__ = "0.18.6-uninstalled"
+    __version__ = "0.18.7-uninstalled"
